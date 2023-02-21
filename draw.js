320718068954439e4d6ea2437a5e7b00fb0e6381
Athan Zhang <athanzhang@Athans-MacBook-Pro.local>
2023-02-21 15:25:43 -0500

    Change brightness settings

--- draw.js
+++ draw.js
@@ -1,17 +1,18 @@
 var video;
 
-var vScale = 16;
+var vScale = 8;
 
 function setup() {
-  createCanvas(640, 480);
+  createCanvas(800, 600);
   pixelDensity(1);
   video = createCapture(VIDEO);
   video.size(width / vScale, height / vScale);
 }
 
 function draw() {
-  background(51);
+  background(255);
   video.loadPixels();
+  
   for (var y = 0; y < video.height; y++) {
     for (var x = 0; x < video.width; x++) {
       var index = (video.width - x + 1 + (y * video.width)) * 4;
@@ -19,8 +20,9 @@ function draw() {
       var g = video.pixels[index + 1];
       var b = video.pixels[index + 2];
       var bright = (r + g + b) / 3;
+
       noStroke();
-      fill(255);
+      fill(0);
       rectMode(CENTER);
       if (bright < 30) {
         text("思", x * vScale, y * vScale);
@@ -28,8 +30,10 @@ function draw() {
         text("心", x * vScale, y * vScale);
       } else if (bright < 170) {
         text("门", x * vScale, y * vScale);
-      } else {
+      } else if (bright < 200) {
         text("一", x * vScale, y * vScale);
+      } else {
+        text(".", x * vScale, y * vScale);
       }
     }
   }
